238f330e7991cce6cf428376a6c51b6c8ef9f234
Tom Mairs <tom.mairs@messagesystems.com>
2020-03-16 17:47:31 -0600

    Complete rewrite.  Getting closer.

--- snippets/recipient-validation/validateRecipient.pl
+++ snippets/recipient-validation/validateRecipient.pl
@@ -6,37 +6,37 @@ use strict;
 use warnings;
 
 use LWP::UserAgent;
+use HTTP::Request;
+use JSON;
 my $url = "https://api.sparkpost.com/api/v1/recipient-validation/single/";
 my $apiKey =  $ENV{'SPARKPOST_API_KEY'};  # API key from environment variable. Should have Recipient Validation rights
 my $recipient = "test\@gmail.com";
 my $shorturl = "api.sparkpost.com";
+my $certpath = "/etc/pki/tls/certs/";
 
 
-my (%params) = @_;
-my $ua = LWP::UserAgent->new;
-my $req = HTTP::Request->new(
-    'GET' =>
-      $url.$recipient,
-    [
-        'Authorization'  => '$apiKey',
-        'Accept'         => 'application/json',
-        'Host'           => '$shorturl:443',
-        'User-Agent'     => 'pcurl/1.0',
-    ],
+# specify a CA path
+my $ua = LWP::UserAgent->new(
+    ssl_opts => {
+        SSL_ca_path     => "$certpath",
+        verify_hostname => 1,
+    },
+    agent => "Perl API Client/1.0"
 );
 
 
-$req->header();
-my $resp = $ua->request($req);
-if ($resp->is_success) {
-  my $message = $resp->decoded_content;
+my @headers = (
+   'Authorization' => "$apiKey");
+
+my $res = $ua->get($url.$recipient,@headers);
+
+if ($res->is_success) {
+  my $message = $res->decoded_content;
   my $json = JSON->new;
   my $data = $json->decode($message);
   return $data;
 }
 else {
-print "HTTP GET error code: ", $resp->code, "\n";
-print "HTTP GET error message: ", $resp->message, "\n";
+print "HTTP GET error code: ", $res->code, "\n";
+print "HTTP GET error message: ", $res->message, "\n";
 }
-
-__END__
