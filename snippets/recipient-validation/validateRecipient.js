e7b728fea97decc112bc72af045161e9818b727e
tuck1s <steve.tuck@sparkpost.com>
2020-05-06 10:36:40 +0100

    Removed old js requests. Added package*.json

--- snippets/recipient-validation/validateRecipient.js
+++ snippets/recipient-validation/validateRecipient.js
@@ -1,24 +1,13 @@
 #!/usr/bin/env node
 "use strict";
-const request = require('request');
+
 //set to api.eu.sparkpost.com for EU accounts
 const url = 'https://api.sparkpost.com/api/v1/recipient-validation/single/';
 const apiKey = process.env.SPARKPOST_API_KEY; // API key from environment variable. Should have Recipient Validation rights
 
 const recipient = 'test@gmail.com';
 
-request.get({
-    url: url + recipient,
-    headers: {
-        'Accept': 'application/json',
-        'Authorization': apiKey
-    }
-}, function (error, response, body) {
-    console.log("\nRequest:")
-    console.error('error:', error);
-    console.log('statusCode:', response && response.statusCode);
-    console.log('body:', body);
-});
+// removed old request library example
 
 // newer Axios library
 const axios = require('axios');
